5fd10edd0f7cafbaa621db99587f96496728266a
Alex <sanches.11vision@gmail.com>
2014-11-26 13:01:00 +0300

    test binding

--- WpfCopyApplication/MainWindow.xaml.cs
+++ WpfCopyApplication/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,21 +14,129 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
 
 namespace WpfCopyApplication
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = this;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // usual OnPropertyChanged implementation
+
+        private string _oldNamespace;
+        private string _newNamespace;
+        private string _sourceDir;
+        private string _backupDir;
+
+        public string OldNamespace
+        {
+            get { return _oldNamespace; }
+            set
+            {
+                if (value != _oldNamespace)
+                {
+                    _oldNamespace = value;
+                    OnPropertyChanged("OldNamespace");
+                }
+            }
+        }
+        public string NewNamespace
+        {
+            get { return _newNamespace; }
+            set
+            {
+                if (value != _newNamespace)
+                {
+                    _newNamespace = value;
+                    OnPropertyChanged("NewNamespace");
+                }
+            }
+        }
+        public string SourceDir
+        {
+            get { return _sourceDir; }
+            set
+            {
+                if (value != _sourceDir)
+                {
+                    _sourceDir = value;
+                    OnPropertyChanged("SourceDir");
+                }
+            }
+        }
+        public string BackupDir
+        {
+            get { return _backupDir; }
+            set
+            {
+                if (value != _backupDir)
+                {
+                    _backupDir = value;
+                    OnPropertyChanged("BackupDir");
+                }
+            }
         }
 
-        private void Browise_Click(object sender, RoutedEventArgs e)
+        void OnPropertyChanged(string propName)
         {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(
+                    this, new PropertyChangedEventArgs(propName));
+        }
+
+
+        private void BrowiseSource_Click(object sender, RoutedEventArgs e)
+        {
+//            OpenFileDialog openFileDialog = new OpenFileDialog();
+//
+//            openFileDialog.InitialDirectory = @"C:\";
+//            openFileDialog.Title = "Browse Text Files";
+//
+//            openFileDialog.CheckFileExists = true;
+//            openFileDialog.CheckPathExists = true;
+//
+//            openFileDialog.DefaultExt = "txt";
+//            openFileDialog.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+//            openFileDialog.FilterIndex = 2;
+//            openFileDialog.RestoreDirectory = true;
+//
+//            openFileDialog.ReadOnlyChecked = true;
+//            openFileDialog.ShowReadOnly = true;
+//
+//            if (openFileDialog.ShowDialog() == true)
+//            {
+//                SourceDir = openFileDialog.FileName;
+//            }
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.FileName = "Document"; // Default file name
+            dlg.DefaultExt = ".txt"; // Default file extension
+            dlg.Filter = "Text documents (.txt)|*.txt"; // Filter files by extension 
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results 
+            if (result == true)
+            {
+                // Open document 
+//                string filename = dlg.FileName;
+                SourceDir = dlg.FileName;
+            }
+        }
+
+        private void BrowiseTarget_Click(object sender, RoutedEventArgs e)
+        {
+
             Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
             dlg.FileName = "Document"; // Default file name
             dlg.DefaultExt = ".txt"; // Default file extension
@@ -39,8 +149,16 @@ namespace WpfCopyApplication
             if (result == true)
             {
                 // Open document 
-                string filename = dlg.FileName;
+                //                string filename = dlg.FileName;
+                BackupDir = dlg.FileName;
             }
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            ReplaceNamespace x = new ReplaceNamespace();
+            x.CopyFile(SourceDir, BackupDir);
+            x.ReplacePartOfFile(BackupDir, OldNamespace, NewNamespace);
         }
     }
 }
---
+++ WpfCopyApplication/ReplaceNamespace.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCopyApplication
+{
+    public class ReplaceNamespace
+    {
+        public ReplaceNamespace()
+        {
+            
+        }
+        public void CopyFile(string sourceDir, string backupDir)
+        {
+            try
+            {
+                string[] txtList = Directory.GetFiles(sourceDir);
+
+                // Copy text files.
+                foreach (string f in txtList)
+                {
+
+                    // Remove path from the file name.
+                    string fName = f.Substring(sourceDir.Length + 1);
+
+                    try
+                    {
+                        // Will not overwrite if the destination file already exists.
+                        File.Copy(Path.Combine(sourceDir, fName), Path.Combine(backupDir, fName));
+                    }
+
+                    // Catch exception if the file was already copied.
+                    catch (IOException copyError)
+                    {
+                        Console.WriteLine(copyError.Message);
+                    }
+                }
+
+                // Delete source files that were copied.
+//                foreach (string f in txtList)
+//                {
+//                    File.Delete(f);
+//                }
+            }
+
+            catch (DirectoryNotFoundException dirNotFound)
+            {
+                Console.WriteLine(dirNotFound.Message);
+            }
+        }
+
+        public void ReplacePartOfFile(string sourceDir, string oldNamespace, string newNamespace)
+        {
+            String strFile = File.ReadAllText(sourceDir);
+
+            strFile = strFile.Replace(oldNamespace, newNamespace);
+
+            File.WriteAllText(sourceDir, strFile);
+        }
+    }
+}
